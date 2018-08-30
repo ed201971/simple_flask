c683de319420980911854b6a5faaa972856e3273
Edward Butler <ted@naxxus.net>
2018-08-30 11:37:00 +0100

    actual code change

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import sys, encodings
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello test5!"
+    return "Hello "+str(sys.argv[1:])
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
